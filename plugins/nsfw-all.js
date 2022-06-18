25edc0b7996495cd727880f33a911a0788778fb1
RexxinOfficial <kenonwa446@gmail.com>
2022-06-18 21:34:16 +0700

    Yhaha Whh

---
+++ plugins/nsfw-all.js
@@ -0,0 +1,10 @@
+let handler = async (m, { conn, command }) => {
+let nyenye = `https://hmtai.herokuapp.com/nsfw/${command}`
+    conn.sendButtonImg(m.chat, nyenye, 'Nih', wm2, 'Next', `.${command}`, m) 
+}
+handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'cum', 'ero', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'jahy', 'maid', 'manga', 'neko', 'orgy', 'panties', 'pussy','sfwneko', 'tentacles', 'thighs', 'yuri']
+handler.tags = ['nsfw']
+handler.command = /^(ahegao|ass|bdsm|blowjob|cum|ero|femdom|foot|gangbang|glasses|hentai|jahy|maid|manga|neko|orgy|panties|pussy|sfwneko|tentacles|thighs|yuri)$/i
+//buatan hyzer, jgn hapus atuh 😊
+handler.premium = true
+module.exports = handler
